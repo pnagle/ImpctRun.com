5dce2c67d0ac0099818fb8fae13629c6314d01fc
akashnautiyal013 <akash.nautiyal013@gmail.com>
2016-05-26 18:15:47 +0530

    someMoreChanges
    Additional changes

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -19,7 +19,7 @@ function HomeController($scope) {
     function init() {
         window.addEventListener('scroll', function(e) {
             var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-                shrinkOn = 100,
+                shrinkOn = 80,
                 header1 = document.querySelector("header1");
             if (distanceY > shrinkOn) {
                 classie.add(header1, "smaller");
