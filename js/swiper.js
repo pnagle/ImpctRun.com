9951181fe81e739c45e6135fbd5e69ba9aa7f555
akashnautiyal013 <akash.nautiyal013@gmail.com>
2016-06-12 16:18:17 +0530

    swiperLoop
    SwiperLoopFixed

--- js/swiper.js
+++ js/swiper.js
@@ -147,7 +147,7 @@
             preloadImages: true,
             updateOnImagesReady: true,
             // loop
-            loop: false,
+            loop: true,
             loopAdditionalSlides: 0,
             loopedSlides: null,
             // Control
